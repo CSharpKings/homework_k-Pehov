87f8ec44dfca4360a2c7e5ddb1f72d81ba2929c9
Pehov <34743440+Pehov@users.noreply.github.com>
2018-01-24 23:06:34 +0300

    2у

---
+++ 09/DZ_9/DZ_9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int proizvedenie = 0;
+            while (true)
+            {
+                Console.WriteLine("Ввидите число");
+                int number = int.Parse(Console.ReadLine());
+                
+                proizvedenie = number * number;
+                    Console.WriteLine(proizvedenie);
+            }
+        }
+    }
+}
---
+++ 09/DZ_9_2/DZ_9_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int MaxNumber = 10;
+            while (true)
+            {
+                int A = rand.Next(MaxNumber);
+                int B = rand.Next(MaxNumber);
+                int C = rand.Next(MaxNumber);
+                int Summa = A + B + C;
+                Console.WriteLine("Сколько будет " + A + " + " + B + " + " + C + " ?");
+                int guess = int.Parse(Console.ReadLine());
+                if (Summa == guess)
+                {
+                    Console.WriteLine("всё правильно!");
+                }
+                else {
+                    Console.WriteLine("Ошибочка. Правильный ответ "+Summa);
+                }
+            }
+
+              
+        }
+    }
+}
