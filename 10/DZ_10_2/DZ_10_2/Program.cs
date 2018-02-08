79715cc36f2e038e1591b0c6a192afb6191a79c8
Pehov <34743440+Pehov@users.noreply.github.com>
2018-02-08 18:03:54 +0300

    cem_10/11

---
+++ 10/DZ_10_2/DZ_10_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cem_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            while (true)
+            {
+                Console.WriteLine("ВВЕДИТЕ ЧИСЛО!");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(number);
+                if (number > max)
+                {
+                    max = number;
+                }
+                Console.WriteLine("Максимальное число " + max);
+
+                
+            }
+
+        }
+    }
+}
