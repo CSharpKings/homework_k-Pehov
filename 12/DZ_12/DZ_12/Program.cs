fa90f9dfc79f884e5467d72ee0ba7f75290d07fb
Pehov <34743440+Pehov@users.noreply.github.com>
2018-02-22 18:03:06 +0300

    Cem_13

---
+++ 12/DZ_12/DZ_12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DZ_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int MaxNumber = 100;
+            int sum = 0;
+            for (int number = 1; number <= 50 ; number++)
+            {
+                int randNumber = rand.Next(MaxNumber);
+                Console.WriteLine(randNumber);
+                sum = sum + randNumber;
+            }
+            int average;
+            average = sum / 50;
+            Console.WriteLine("среднее арифметическое " + average);
+            Thread.Sleep(10000);
+
+        }
+    }
+}
---
+++ 12/DZ_12/DZ_12_2/DZ_12_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DZ_12_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int high = 0;
+            for (int boyar = 1; boyar <= 20; boyar++)
+            {
+                Console.WriteLine("Введите рост боярина");
+                int growing = int.Parse(Console.ReadLine());
+                if (growing > 160)
+                {
+                    high++;
+                }
+
+            }
+            Console.WriteLine("должны подвергнуться наказанию " + high + " боярина");
+            Thread.Sleep(10000);
+        }
+    }
+}
