bd5806d627ce5756db637a5cbdb69a951f3c3d11
Pehov <34743440+Pehov@users.noreply.github.com>
2018-03-08 18:10:07 +0300

    dz_13

---
+++ 13/DZ_13/dz_13_1/dz_13_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dz_13_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово.");
+            string str = Console.ReadLine();
+            int strlenght = str.Length;
+            if (str[0] == str[str.Length - 1])
+            {
+                Console.WriteLine("Мне нравится!");
+            }
+            else
+            {
+                Console.WriteLine("Мне не нравится!");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
