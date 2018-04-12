f1421bf8528f0b7a4eb9fef6b6f60a44f1ee3219
Pehov <34743440+Pehov@users.noreply.github.com>
2018-04-12 18:00:41 +0300

    DZ_2_15_7

---
+++ 15/DZ2_15_7/DZ2_15_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DZ2_15_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] monthJuly = { 5, 4, 3, 6, 2, 9, 3, 10, 5, 2 };
+            int July = 0;
+            for (int i = 0; i < monthJuly.Length; i++)
+            {
+                July = July + monthJuly[i];
+            }
+
+            int[] monthAugust = { 7, 2, 4, 5, 1, 9, 8, 3, 6, 10 };
+            int August = 0;
+            for (int i2 = 0; i2 < monthAugust.Length; i2++)
+            {
+                August = August + monthAugust[i2];
+            }
+
+            int common;
+            common = July + August;
+
+            Console.WriteLine(common);
+            Console.ReadLine();
+        }
+    }
+}
